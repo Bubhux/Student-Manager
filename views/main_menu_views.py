526dbe6cae06fea0fbe331f83af7e0a2641330d4
Bubhux <bubhuxpaindepice@gmail.com>
2024-04-02 23:53:36 +0200

    Mise à jour V.2.9  Changes to be committed: 	modified:   controllers/classroom_controller.py 	modified:   requirements.txt 	modified:   views/main_menu_views.py

--- views/main_menu_views.py
+++ views/main_menu_views.py
@@ -1,9 +1,14 @@
+import click
+from rich.console import Console
+from rich.table import Table
+
 from controllers.student_controller import StudentDatabaseController
 from controllers.classroom_controller import ClassroomDatabaseController
-
 from views.student_menu_views import StudentView
 from views.classroom_menu_views import ClassroomView
 
+
+console = Console()
 
 class MainMenuView:
 
@@ -16,21 +21,27 @@ class MainMenuView:
     def display_main_menu(self):
 
         while True:
-            print("\nMenu principal")
-            print("1. Gestion des étudiants")
-            print("2. Gestion des classes")
-            print("3. Quitter le programme\n")
+            table = Table(show_header=True, header_style="bold magenta")
+            table.add_column("Choix")
+            table.add_column("Action")
+            table.add_row("1", "Gestion des étudiants", style="cyan")
+            table.add_row("2", "Gestion des classes", style="cyan")
+            table.add_row("3", "Quitter le programme", style="cyan")
 
-            choice_menu = input("Choisissez le numéro de votre choix.\n> ")
+            # Ajoute une chaîne vide avant le titre pour simuler l'alignement à gauche
+            console.print()
+            console.print(" Menu principal", style="bold magenta")
 
-            if choice_menu == "1":
+            console.print(table)
+
+            choice_menu = click.prompt("Choisissez le numéro de votre choix.", type=int)
+
+            if choice_menu == 1:
                 self.student_view.display_main_menu()
-
-            elif choice_menu == "2":
+            elif choice_menu == 2:
                 self.classroom_view.display_main_menu()
-
-            elif choice_menu == "3":
-                print("Merci d'avoir utilisé ce programme !")
+            elif choice_menu == 3:
+                console.print("Merci d'avoir utilisé ce programme !")
                 break
             else:
-                print("Choix invalide, saisissez un nombre entre 1 et 3.")
+                console.print("Choix invalide, saisissez un nombre entre 1 et 3.", style="bold red")
