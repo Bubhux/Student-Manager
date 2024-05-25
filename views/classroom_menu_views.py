645e6a59af2c08fcda3a9f8cbd04d5b0b2f9f1a2
Bubhux <bubhuxpaindepice@gmail.com>
2024-05-25 08:04:25 +0200

    Mise à jour V.7.7  Changes to be committed: 	modified:   views/classroom_menu_views.py

--- views/classroom_menu_views.py
+++ views/classroom_menu_views.py
@@ -308,6 +308,7 @@ class ClassroomView:
                         table = Table(title="Liste des étudiants dans la classe triés par ordre alphabétique")
                         table.add_column("Numéro", justify="right")
                         table.add_column("Prénom", justify="left")
+                        table.add_column("Nom", justify="left")
                         for index, student in enumerate(sorted_students, start=1):
                             print(f"{index}. {student['first_name']} {student['last_name']}")
 
