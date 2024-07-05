d22e03dbc6ca192f74691f396bb78937e0356f51
Bubhux <bubhuxpaindepice@gmail.com>
2024-07-05 21:27:37 +0200

    Mise à jour V.11.8  Changes to be committed: 	modified:   views/classroom_menu_views.py

--- views/classroom_menu_views.py
+++ views/classroom_menu_views.py
@@ -361,6 +361,7 @@ class ClassroomView:
 
             # Crée et affiche le tableau de résumé
             table = Table(show_header=True, header_style="bold magenta")
+            table.add_column("Champ", style="cyan")
 
             confirmation_message = click.style("Confirmez-vous la création de cette classe ?", fg="yellow")
             if click.confirm(confirmation_message, default=True):
