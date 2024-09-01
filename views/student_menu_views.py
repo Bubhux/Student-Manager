2b4c6a653b5ae282bb8438f9cc7f1ea07d838da5
Bubhux <bubhuxpaindepice@gmail.com>
2024-09-01 10:37:16 +0200

    Mise à jour V.17.6  Changes to be committed: 	modified:   views/student_menu_views.py

--- views/student_menu_views.py
+++ views/student_menu_views.py
@@ -399,6 +399,11 @@ class StudentView:
         table.add_column("Nom et prénom", style="cyan")
         table.add_column("Classe", style="cyan")
 
+        # Remplis le tableau avec les informations des étudiants
+        for index, student in enumerate(sorted_students, start=1):
+            student_name = f"{student['first_name']} {student['last_name']}"
+            classroom_name = student.get('classroom_name', 'N/A')
+
     def calculate_student_average(self):
         # Récupére tous les étudiants de la base de données
         students = self.student_controller.get_all_students_database_controller()
